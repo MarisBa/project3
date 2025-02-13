79a5b9275229f55176d1739c44a6291dc2b51b18
MarisBa <marisbalcuns123@gmail.com>
2025-02-13 22:04:14 +0200

    1stcommit

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('/', function(){
+    return view('welcome');
+
+});
+
+
+Route::get('about','Testcontroller@index');
+
